77c500d16c411db911242f08cc14521acdf5376f
James Johnson <jjohnsonjj1251@gmail.com>
2024-11-20 19:42:53 -0500

    Use size parameter in `map_memory`
    This aligns the usage of memory mappings with other functions
    in the library.

--- src/bin/gamozo_test.rs
+++ src/bin/gamozo_test.rs
@@ -7,9 +7,9 @@ use softmew::map::Perm;
 
 pub fn main() {
     let mut mew = MMU::new();
-    mew.map_memory(0x100, 0x104, Perm::default()).unwrap();
+    mew.map_memory(0x0, 1024 * 1024, Perm::default()).unwrap();
     let data = mew.get_mapping_mut(0x100).unwrap();
-    data.data_mut().copy_from_slice(b"asdf");
+    data.data_mut()[0..4].copy_from_slice(b"asdf");
 
     let forked = mew.clone();
     for _ in 0..100_000_000 {
